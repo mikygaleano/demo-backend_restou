2c54a87f96c832762d3dc73cca53c87cf900c7ae
mikygaleano <mikebostero.bernal@gmail.com>
2024-08-04 22:00:44 -0300

    add errorMiddleware

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors';
 
 import productsRoute from './routes/productsRoute.js'
+import { errorMiddleware } from './middleware/errorMiddleware.js';
 
 const app = express();
 
 app.use(cors());
+
+app.use(errorMiddleware)
 
 app.use('/', productsRoute)
 
